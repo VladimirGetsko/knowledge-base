f53902179f8243bf19ed17cbfd9ef064355aa931
Volodymyr <getskowork@gmail.com>
2024-06-19 19:48:19 +0300

    Added login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 
 export const Header = () => {
-  const isAuth = false;
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth)
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if(window.confirm('Вы действительно хотите выйти?')) {
+      dispatch(logout());
+      window.localStorage.removeItem('token')
+    }
+  };
 
   return (
     <div className={styles.root}>
